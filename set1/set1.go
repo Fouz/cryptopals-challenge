4ee64e162e0bb9cad76aca6ba4e2aeb90ba8a028
Fouz <a321ak@gmail.com>
2025-05-04 14:10:17 +0300

    Problem 3

--- set1/set1.go
+++ set1/set1.go
@@ -1,9 +1,12 @@
 package cryptopals
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 // Q1
@@ -42,4 +45,75 @@ func XOR(b1, b2 []byte) ([]byte, error) {
 	}
 
 	return res, nil
+}
+
+// Q3
+func ReadText(url string) map[byte]float64 {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return ScoreCharacters(body)
+}
+
+// func ReadText(name string) map[byte]float64 {
+// 	text, err := os.ReadFile(name)
+// 	if err != nil {
+// 		fmt.Errorf("failed to open file: %w", err)
+// 	}
+// 	return ScoreCharacters(text)
+// }
+
+func ScoreCharacters(text []byte) map[byte]float64 {
+
+	frequencyMap := make(map[byte]float64)
+
+	totalCount := float64(len(text))
+
+	for _, val := range text {
+		frequencyMap[val]++
+	}
+
+	for key, val := range frequencyMap {
+		frequencyMap[key] = val / totalCount
+	}
+
+	return frequencyMap
+}
+
+func Score(b []byte, f map[byte]float64) float64 {
+	var score float64
+	for _, val := range b {
+		score += f[val]
+	}
+	return score / float64(len(b))
+}
+
+func SingleByteXOR(b []byte, key byte) []byte {
+
+	fullKey := bytes.Repeat([]byte{key}, len(b))
+	res, _ := XOR(fullKey, b)
+	return res
+
+}
+
+func FindKey(e []byte, f map[byte]float64) []byte {
+	var res []byte
+	var lastScore float64
+
+	for key := byte(0); key < 255; key++ {
+		out := SingleByteXOR(e, key)
+		score := Score(out, f)
+		if score > lastScore {
+			res = out
+			lastScore = score
+		}
+	}
+	return res
 }
--- set1/set1_test.go
+++ set1/set1_test.go
@@ -2,6 +2,7 @@ package cryptopals
 
 import (
 	"bytes"
+	"encoding/hex"
 	"testing"
 )
 
@@ -11,10 +12,11 @@ func TestQ1(t *testing.T) {
 
 		// invalid hex contains 'n' exp
 		hexString := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6n"
-		_, err := DecodeHexToBase64(hexString)
+		str, err := DecodeHexToBase64(hexString)
 		if err == nil {
 			t.Fatalf("This is invalid hex expeted to fail")
 		}
+		t.Log(str)
 	})
 
 	t.Run("it should decode valid hex to correct base64 string", func(t *testing.T) {
@@ -31,6 +33,9 @@ func TestQ1(t *testing.T) {
 }
 
 func TestXORHexStrings(t *testing.T) {
+
+	// TODO: use generics
+
 	aHex := "1c0111001f010100061a024b53535009181c"
 	bHex := "686974207468652062756c6c277320657965"
 	expectedHex := "746865206b696420646f6e277420706c6179"
@@ -58,4 +63,23 @@ func TestXORHexStrings(t *testing.T) {
 	if !bytes.Equal(result, expected) {
 		t.Errorf("unexpected XOR result:\n got: %x\nwant: %x", result, expected)
 	}
+}
+
+func TestProblem3(t *testing.T) {
+	frequencyMap := ReadText("http://gutenberg.org/cache/epub/98/pg98.txt")
+	for k, v := range frequencyMap {
+		t.Logf("%c: , %.5f", k, v)
+	}
+	encryptedMsg, err := HexDecode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	if err != nil {
+		t.Fatalf("failed to decode hex string a: %v", err)
+	}
+
+	res := FindKey(encryptedMsg, frequencyMap)
+	decryptedMsg := hex.EncodeToString(res)
+	bs, err := hex.DecodeString(decryptedMsg)
+	if err != nil {
+		panic(err)
+	}
+	t.Log(string(bs))
 }
